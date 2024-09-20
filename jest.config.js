9b21492ead68d036b5aa4998888b086859ae0f49
liziane cysne <lizianecysne1@gmail.com>
2024-09-20 11:54:26 -0300

    feat: Adiciona arquivo de configuração do Jest

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+// jest.config.js
+module.exports = {
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest',
+  },
+  transformIgnorePatterns: ['/node_modules/'],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css|less)$': '<rootDir>/__mocks__/fileMock.js',
+  },
+  testEnvironment: 'jsdom',
+};
