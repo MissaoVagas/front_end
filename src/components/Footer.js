ba3426369f80f73428256be116f2c41986b77c53
liziane cysne <lizianecysne1@gmail.com>
2024-09-20 11:57:29 -0300

    feat: Adiciona componente Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+// components/Footer.js
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="footer p-5 bg-gray-800 text-center text-white shadow-md">
+      <p>&copy; 2023 JobsRocket. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
