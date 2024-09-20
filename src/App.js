f9d9a28d617bc14e438888b28c33adbb70cda6f8
liziane cysne <lizianecysne1@gmail.com>
2024-09-20 11:56:46 -0300

    feat: Adiciona componente ImpulsioneCarreira e atualiza estrutura do aplicativo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+// App.js
+import React from 'react';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Categorias from './components/Categorias';
+import EmpresasDestaque from './components/EmpresasDestaque';
+import ImpulsioneCarreira from './components/ImpulsioneCarreira';
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <main className="main-content">
+        <Hero />
+        <Categorias />
+        <EmpresasDestaque />
+        <ImpulsioneCarreira />
+      </main>
+      <Footer />
     </div>
   );
 }
