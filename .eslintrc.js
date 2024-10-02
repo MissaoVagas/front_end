a7349810e0c61103bf626dd065ee3a4b85410c87
liziane cysne <lizianecysne1@gmail.com>
2024-10-02 14:38:52 -0300

    feat:Adiciona regra de validação para âncoras inválidas no arquivo de configuração do ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    // Outras configurações
+    rules: {
+      // Outras regras
+      'jsx-a11y/anchor-is-valid': [
+        'warn',
+        {
+          aspects: ['invalidHref'],
+        },
+      ],
+    },
+  };
