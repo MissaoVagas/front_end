f56475786487ca924932f30f34583cdbf3951d54
liziane cysne <liziane.medeiros@sefin.fortaleza.ce.gov.br>
2024-10-04 09:24:53 -0300

    refactor: Atualiza configuração do arquivo .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,28 @@
 module.exports = {
-  plugins: [
-    'jsx-a11y',
-  ],
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    requireConfigFile: false,
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  env: {
+    browser: true,
+    node: true,
+    jest: true 
+  },
   extends: [
-    'plugin:jsx-a11y/recommended',
+    'eslint:recommended',
+    'plugin:react/recommended'
   ],
   rules: {
     'jsx-a11y/anchor-is-valid': 'off', // Desativa a regra temporariamente, durante o desenvolvimento
   },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  }
 };
