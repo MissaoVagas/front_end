941a934ea74770c54a33291be3e835f9e844a7b0
Liziane Cysne <lizianecysne1@gmail.com>
2024-10-25 16:08:50 -0300

    Atualiza configurações do ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,28 @@
 module.exports = {
-  parser: '@babel/eslint-parser',
+  parser: "@babel/eslint-parser",
   parserOptions: {
     requireConfigFile: false,
     ecmaVersion: 2020,
-    sourceType: 'module',
+    sourceType: "module",
     ecmaFeatures: {
-      jsx: true
-    }
+      jsx: true,
+    },
   },
   env: {
     browser: true,
     node: true,
-    jest: true 
+    jest: true,
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended'
-  ],
+  extends: ["eslint:recommended", "plugin:react/recommended"],
   rules: {
-    'jsx-a11y/anchor-is-valid': 'off', // Desativa a regra temporariamente, durante o desenvolvimento
-    'react/react-in-jsx-scope': 'off'  // Desativa a regra que exige React no escopo ao usar JSX
+    "jsx-a11y/anchor-is-valid": "off", // Desativa a regra temporariamente, durante o desenvolvimento
+    "react/react-in-jsx-scope": "off", // Desativa a regra que exige React no escopo ao usar JSX
+    "react/prop-types": "off", // Desativa a regra de validação de prop-types
+    "no-unused-vars": "off", // Desativa a regra de variáveis não utilizadas
   },
   settings: {
     react: {
-      version: 'detect'
-    }
-  }
+      version: "detect",
+    },
+  },
 };
