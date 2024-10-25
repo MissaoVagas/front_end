de4e242f0d159c879d1b6e8d3f9df72282d9a401
Liziane Cysne <lizianecysne1@gmail.com>
2024-10-25 16:16:09 -0300

    Atualiza Navbar.js para seguir padrões de formatação

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,10 @@
 // src/components/Navbar.js
-import React, { useState } from 'react';
-import logo from '../assets/icons/logo.png';
-import './Navbar.css';
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import logo from "../assets/icons/logo.png";
+import "./Navbar.css";
 
 function Navbar({ isLoginVisible, menuItems }) {
-
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleMenu = () => {
@@ -19,14 +19,33 @@ function Navbar({ isLoginVisible, menuItems }) {
           <span className="text-2xl text-white-500">JobsRocket</span>
         </div>
         <div className="md:hidden">
-          <button onClick={toggleMenu} className="text-white focus:outline-none" aria-label="menu">
-            <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
+          <button
+            onClick={toggleMenu}
+            className="text-white focus:outline-none"
+            aria-label="menu"
+          >
+            <svg
+              className="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16m-7 6h7"
+              ></path>
             </svg>
           </button>
         </div>
-        <ul className={`flex-col md:flex-row md:flex md:items-center md:space-x-5 ${isOpen ? 'flex' : 'hidden'} absolute md:static navbar md:bg-transparent w-full md:w-auto left-0 top-16 md:top-auto`}>
-        {menuItems.map((item, index) => (
+        <ul
+          className={`flex-col md:flex-row md:flex md:items-center md:space-x-5 ${
+            isOpen ? "flex" : "hidden"
+          } absolute md:static navbar md:bg-transparent w-full md:w-auto left-0 top-16 md:top-auto`}
+        >
+          {menuItems.map((item, index) => (
             <li key={index} className="text-center py-2">
               <a href={item.link} className="text-white hover:text-blue-400">
                 {item.label}
@@ -35,7 +54,9 @@ function Navbar({ isLoginVisible, menuItems }) {
           ))}
           {isLoginVisible && (
             <li className="text-center py-2 md:hidden">
-              <button className="bg-purple-500 hover:bg-blue-400 text-white py-2 px-4 rounded transition duration-300">Log in</button>
+              <button className="bg-purple-500 hover:bg-blue-400 text-white py-2 px-4 rounded transition duration-300">
+                Log in
+              </button>
             </li>
           )}
         </ul>
@@ -48,5 +69,15 @@ function Navbar({ isLoginVisible, menuItems }) {
     </nav>
   );
 }
+
+Navbar.propTypes = {
+  isLoginVisible: PropTypes.bool.isRequired,
+  menuItems: PropTypes.arrayOf(
+    PropTypes.shape({
+      label: PropTypes.string.isRequired,
+      link: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+};
 
 export default Navbar;
