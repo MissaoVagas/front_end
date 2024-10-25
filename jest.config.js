1a279ff227f1b36437f00b6f366c6c8691772d64
Liziane Cysne <lizianecysne1@gmail.com>
2024-10-25 16:09:13 -0300

    Atualiza configurações do Jest para seguir padrões de formatação

--- jest.config.js
+++ jest.config.js
@@ -1,21 +1,21 @@
 module.exports = {
-  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+  setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"],
   transform: {
-    '^.+\\.jsx?$': 'babel-jest',
+    "^.+\\.jsx?$": "babel-jest",
   },
-  transformIgnorePatterns: ['/node_modules/'],
+  transformIgnorePatterns: ["/node_modules/"],
   moduleNameMapper: {
-    '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js',
-    '\\.(css|less)$': '<rootDir>/__mocks__/fileMock.js',
+    "\\.(jpg|jpeg|png|gif|webp|svg)$": "<rootDir>/__mocks__/fileMock.js",
+    "\\.(css|less)$": "<rootDir>/__mocks__/fileMock.js",
   },
-  testEnvironment: 'jsdom',
-  
+  testEnvironment: "jest-environment-jsdom",
+
   collectCoverage: true,
-  coverageDirectory: 'coverage',
-  coverageReporters: ['json', 'lcov', 'html', 'text'],
+  coverageDirectory: "coverage",
+  coverageReporters: ["json", "lcov", "html", "text"],
   collectCoverageFrom: [
-    'src/**/*.{js,jsx}',
-    '!src/index.js',
-    '!src/reportWebVitals.js',
+    "src/**/*.{js,jsx}",
+    "!src/index.js",
+    "!src/reportWebVitals.js",
   ],
 };
