5099a85353815dc588e418c66e13b2ae6f5802d5
Liziane Cysne <lizianecysne1@gmail.com>
2024-10-25 16:18:05 -0300

    Atualiza testes de renderização do App.js

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,17 +1,15 @@
+import React from "react"; // Adicione esta linha
+import { render, screen } from "@testing-library/react";
+import App from "../App";
 
-
-import React from 'react'; // Adicione esta linha
-import { render, screen } from '@testing-library/react';
-import App from '../App';
-
-test('renders job opportunities text', () => {
+test("renders job opportunities text", () => {
   render(<App />);
   const linkElement = screen.getByText(/empregos/i);
   expect(linkElement).toBeInTheDocument();
 });
 
-test('renders Navbar component', () => {
+test("renders Navbar component", () => {
   render(<App />);
-  const navbarElement = screen.getByRole('navigation');
+  const navbarElement = screen.getByRole("navigation");
   expect(navbarElement).toBeInTheDocument();
 });
